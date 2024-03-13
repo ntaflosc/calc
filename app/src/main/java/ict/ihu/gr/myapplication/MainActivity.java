e276fa8c6b9561574bbf29b8432c46c4f30e67cb
Chris <ntaflosc@gmail.com>
2024-03-13 23:14:49 +0200

    working calc with memory testing req

--- app/src/main/java/ict/ihu/gr/myapplication/MainActivity.java
+++ app/src/main/java/ict/ihu/gr/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +15,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private double memory = 0.0; // Stores current memory value
+    private boolean memorySet = false; // Flag to indicate if memory has a value
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,30 +35,57 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 onCalculateClick();
-
             }
         });
     }
+
     private void onCalculateClick() {
         EditText etNumber1 = findViewById(R.id.etNumber1);
         double number1;
-        try{
+        try {
             number1 = Double.parseDouble(etNumber1.getText().toString());
-        }catch (Exception e){
+        } catch (Exception e) {
             number1 = 0.0;
         }
         EditText etNumber2 = findViewById(R.id.etNumber2);
         double number2;
-        try{
+        try {
             number2 = Double.parseDouble(etNumber2.getText().toString());
-        }catch (Exception e){
+        } catch (Exception e) {
             number2 = 0.0;
         }
         RadioGroup rgOperations = findViewById(R.id.rgOperations);
         TextView tvResult = findViewById(R.id.tvResult);
         int selectedId = rgOperations.getCheckedRadioButtonId();
 
-        if (selectedId == R.id.rbPlus) {
+        if (selectedId == R.id.btnMC) { // Memory Clear
+            memory = 0.0;
+            memorySet = false;
+            Toast.makeText(getApplicationContext(), "Memory Cleared", Toast.LENGTH_SHORT).show();
+        } else if (selectedId == R.id.btnMR) { // Memory Recall
+            if (memorySet) {
+                etNumber1.setText(String.valueOf(memory));
+            } else {
+                Toast.makeText(getApplicationContext(), "Memory Empty", Toast.LENGTH_SHORT).show();
+            }
+        } else if (selectedId == R.id.btnMS) { // Memory Store
+            try {
+                memory = Double.parseDouble(etNumber1.getText().toString());
+                memorySet = true;
+                Toast.makeText(getApplicationContext(), "Value Stored in Memory", Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(), "Invalid Input", Toast.LENGTH_SHORT).show();
+            }
+        } else if (selectedId == R.id.btnMAdd) { // Memory Add
+            try {
+                double currentNumber = Double.parseDouble(etNumber1.getText().toString());
+                memory += currentNumber;
+                memorySet = true;
+                Toast.makeText(getApplicationContext(), "Value Added to Memory", Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(), "Invalid Input", Toast.LENGTH_SHORT).show();
+            }
+        } else if (selectedId == R.id.rbPlus) {
             tvResult.setText(String.valueOf(number1 + number2));
         } else if (selectedId == R.id.rbMinus) {
             tvResult.setText(String.valueOf(number1 - number2));
@@ -69,6 +100,9 @@ public class MainActivity extends AppCompatActivity {
             tvResult.setText("Operation Not Selected");
         }
 
-    }
 
+    }
 }
+
+
+
